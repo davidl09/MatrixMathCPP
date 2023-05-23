11ae6e0b4a57a9be0a7476a00efe7cb7e5c77947
davidl09 <114118694+davidl09@users.noreply.github.com>
2023-05-23 09:48:04 -0400

    added tostr, cleanup funcs

--- src/rational.hpp
+++ src/rational.hpp
@@ -5,20 +5,24 @@
 #include <stdexcept>
 #include <vector>
 
+bool isprime(long a);
 std::vector<long int> prime_fact(long int a);
 long int gcf(int a, int b);
 
 class Rational{
     public:
         Rational(int n, int d);
+        Rational(double n, double d);
+        Rational(double r);
+        Rational(std::string str);
         Rational operator+(Rational a);
         Rational operator-(Rational a);
         Rational operator*(Rational a);
         Rational operator/(Rational a);
-        long int data();
         long int numer();
         long int denom();
         void cleanup();
+        std::string tostr();
         double dec_est();
     private:
         long int numerator;
